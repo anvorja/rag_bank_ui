4a79fd5c906d491a19553dac42a4574989b553a6
anvorja <andres.vorja.vorja@gmail.com>
2025-11-24 23:54:10 -0500

    React import added

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,7 @@
 // src/components/ui/sonner.tsx
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, toast } from "sonner";
+import React from "react";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
